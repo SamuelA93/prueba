bdb8f904ec20009572b2d9234ed130aeaf893637
SamuelAstol <samuel.astol@gmail.com>
2014-07-26 15:18:17 -0300

    Initialized variables 2 type string

--- src/main.java
+++ src/main.java
@@ -4,6 +4,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("concatenar palabras");
+		String saludo, nombre;
+		saludo="Hola !!! ";
+		nombre = "Marcos";
 	}
 
 }
