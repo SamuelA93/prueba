e735bbd42e0bd3ef367e6a52267bb84ac65dc3eb
SamuelAstol <samuel.astol@gmail.com>
2014-07-26 16:07:55 -0300

    new output greeting

--- src/main.java
+++ src/main.java
@@ -4,12 +4,12 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("concatenar palabras");
-		String saludo, nombre;
+		String saludo, nombre,nombre2;
 		saludo="Hola !!! ";
 		nombre = "Marcos";
+		nombre2="Gabriel";
 		
-		
-		
+		System.out.println(saludo+nombre2);
 		System.out.println(saludo+nombre);
 	}
 
